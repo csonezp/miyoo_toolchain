1f727885e0eb682434a06575c43d749de15ad246
steward-fu <steward.fu@gmail.com>
2019-01-31 22:43:26 +0800

    init

---
+++ x64/lib/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/opt/miyoo/lib"
+XSLT_LIBS="-lxslt  -L/opt/miyoo/lib -lxml2 -lm -ldl -lm "
+XSLT_INCLUDEDIR="-I/opt/miyoo/include"
+MODULE_VERSION="xslt-1.1.29"
